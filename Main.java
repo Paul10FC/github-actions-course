91f7f170c554867e5099f90739b3c0e5b4f9ec75
Paul Flores <paulfloresdev@icloud.com>
2023-11-21 17:56:07 -0600

    add the variables

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String user = System.getenv("USERNAME");
+        System.out.println("Hello " + user + " from github world!");
     }
 }
