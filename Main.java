d04fc009ff82161d3b8e129b6c60838a56664f08
Paul Flores <paulfloresdev@icloud.com>
2023-11-21 17:40:28 -0600

    Add the local variables

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String username = System.getenv("USERNAME");
+        System.out.println("Hello " + username + " from your github account");
     }
 }
